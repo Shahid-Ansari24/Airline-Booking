5f5a5b3c9c9ad4e869ba8b6d8042a03c6c1f2850
Shahid <shahidansari2452@gmail.com>
2025-08-29 23:58:16 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const { PORT } = require('./config');
+
+const app = express();
+
+app.listen((req, res) => {
+    res.send(`Successfully started the server ${PORT}`);
+})
